0c596fb1e342bf3e9c3ab0d177a2c3dc78e27e7f
KING SA_ONE <kingsaone7@gmail.com>
2025-01-12 06:12:57 +0530

    login and signup pages done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,8 +7,8 @@ const Home = () => {
        <div className='bg-cover bg-center bg-[url(https://img.freepik.com/premium-photo/clear-red-traffic-lights-ensure-safe-driving-pedestrian-crossing_1168676-35147.jpg)] h-screen pt-8 flex justify-between flex-col w-full'>
         <img className='w-16 ml-8' src="https://www.logo.wine/a/logo/Uber/Uber-White-Dark-Background-Logo.wine.svg" alt="" />
         <div className='bg-white pb-7 py-4 px-4'>
-            <h2 className='text-2xl font-bold'>Get Started With Uber</h2>
-            <Link to='/login' className='flex items-center justify-center w-full bg-black text-white py-3 rounded mt-5'>Continue</Link>
+            <h2 className='text-[30px] font-bold'>Get Started With Uber</h2>
+            <Link to='/login' className='flex items-center justify-center w-full bg-black text-white py-3 rounded-lg mt-5'>Continue</Link>
         </div>
          
        </div>
